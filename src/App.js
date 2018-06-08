ca5fbf130682d6acad724a2b9bfb0445dc56762d
Sarin Udompanit <sarin_udo@sendit.asia>
2018-06-08 10:09:14 +0700

    Setup prettier and eslint

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        YOYO
-      </div>
-    );
+    return <div className="App">YOYO</div>;
   }
 }
 
