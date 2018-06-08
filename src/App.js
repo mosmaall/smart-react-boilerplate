32f621ec97cd8bb1f7605f05f364577a049e6c06
Sarin Udompanit <sarin_udo@sendit.asia>
2018-06-08 10:02:14 +0700

    Setup hot-reloading

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+        YOYO
       </div>
     );
   }
 }
 
-export default App;
+export default hot(module)(App);
