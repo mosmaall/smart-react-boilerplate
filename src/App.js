f90dc420358e961be9fe6de743eb0fc15a078a52
Sarin Udompanit <sarin_udo@sendit.asia>
2018-06-08 11:31:15 +0700

    Add Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
-import { hot } from 'react-hot-loader';
-import './App.css';
+import React, { Component } from 'react'
+import { hot } from 'react-hot-loader'
+import { Provider as StoreProvider } from 'mobx-react'
+import Routes from './Routes'
+import { getInstanceStores } from './stores'
 
 class App extends Component {
   render() {
-    return <div className="App">YOYO</div>;
+    return (
+      <StoreProvider {...getInstanceStores()}>
+        <Routes />
+      </StoreProvider>
+    )
   }
 }
 
-export default hot(module)(App);
+export default hot(module)(App)
---
+++ src/Routes.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Loadable from 'react-loadable'
+import { inject, observer } from 'mobx-react'
+
+const LOADABLE_CONFIG = { loading: () => null, delay: 2000 }
+
+//Dynamic loading
+const Home = {
+  home: Loadable({
+    ...LOADABLE_CONFIG,
+    loader: () =>
+      import(/* webpackChunkName: "route.HomePage" */ './pages/HomePage'),
+  }),
+}
+
+const Routes = () => {
+  return (
+    <div>
+      <Router basename={process.env.REACT_APP_BASE_URL}>
+        <div>
+          <Route exact path="/" component={Home.home} />
+        </div>
+      </Router>
+    </div>
+  )
+}
+
+export default Routes
