442ed6625d79186fa205442953812b372d2ccaa5
Sarin Udompanit <sarin_udo@sendit.asia>
2018-06-14 10:28:27 +0700

    Add apolloProvider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
 import { Provider as StoreProvider } from 'mobx-react'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+
 import Routes from './Routes'
-import { getInstanceStores } from './stores'
+import { getInstanceStores } from './stores/Store'
+
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_API_ENDPOINT,
+})
 
 class App extends Component {
   render() {
     return (
-      <StoreProvider {...getInstanceStores()}>
-        <Routes />
-      </StoreProvider>
+      //Remove ApolloProvider if don't use
+      <ApolloProvider client={client}>
+        <StoreProvider {...getInstanceStores()}>
+          <Routes />
+        </StoreProvider>
+      </ApolloProvider>
     )
   }
 }
