6ef96293e60774792c5735274d3f0f7301e4dc21
Sarin Udompanit <sarin_udo@sendit.asia>
2018-06-14 10:27:11 +0700

    Add theme

---
+++ src/theme/theme.js
@@ -0,0 +1,56 @@
+export const color = {
+  primary: '#303F9F',
+  secondary: '#2196F3',
+  background: '#FFFFFF',
+  textPrimary: '#212121',
+  textSecondary: '#FFFFFF',
+  textPrimaryInvert: '#FFFFFF',
+  inactive: '#FFFFFF',
+  link: '#FFFFFF',
+  sideNav: '#FFFFFF',
+  appHeaderBg: '#FFFFFF',
+  appFooterBg: '#FFFFFF',
+  warning: '#FFFFFF',
+  success: '#FFFFFF',
+  pending: '#FFFFFF',
+  mention: '#FFFFFF',
+  default: '#FFFFFF',
+  danger: '#FFFFFF',
+  forgotPassword: '#FFFFFF',
+  border: '#FFFFFF',
+}
+
+export const font = {
+  display2: '38px',
+  display1: '34px',
+  headline: '30px',
+  title: '20px',
+  subHeading: '16px',
+  body1: '14px',
+}
+
+export const spacing = {
+  unt: 4,
+}
+
+export const dimens = {
+  xs: '4px',
+  sm: '8px',
+  md: '16px',
+  lg: '32px',
+  xl: '64px',
+  appHeaderHeight: '64px',
+  appSidebarWidth: '64px',
+}
+
+const typography = {
+  fontFamily: 'LilyUPC, Kanit, sans-serif',
+}
+
+export default {
+  font,
+  color,
+  spacing,
+  dimens,
+  typography,
+}
