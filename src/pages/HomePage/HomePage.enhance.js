b9e9282d5f7f6c65a67de11bdc920b0846943b35
Sarin Udompanit <sarin_udo@sendit.asia>
2018-07-03 18:34:04 +0700

    Connect store to homePage.enhance

--- src/pages/HomePage/HomePage.enhance.js
+++ src/pages/HomePage/HomePage.enhance.js
@@ -1,8 +1,18 @@
-import { compose, withState, lifecycle, withHandlers } from 'recompose'
-
+import {
+  compose,
+  withState,
+  lifecycle,
+  withHandlers,
+  withReducer,
+  withContext,
+} from 'recompose'
+import { connect } from 'react-redux'
 import HomePage from './HomePage'
 
 export default compose(
+  connect(state => ({
+    cat: state.homePage.cat,
+  })),
   withHandlers({
     onHandleClick: props => e => {
       console.log('e =', e)
