d7c4a748b751fc40a13b270e3537f176cc3ec740
Sarin Udompanit <sarin_udo@sendit.asia>
2018-07-03 10:36:12 +0700

    Add homePageReducer to root reducer

--- src/cores/reducers.js
+++ src/cores/reducers.js
@@ -1,3 +1,6 @@
 import { combineReducers } from 'redux'
+import homePage from '../pages/HomePage/HomePage.reducer'
 
-export default combineReducers({})
+export default combineReducers({
+  homePage,
+})
